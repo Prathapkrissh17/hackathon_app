893a09ce45d248569ae009cb1e4b02c4b293e93c
Prathap <prathap@thirantech.com>
2024-12-21 10:32:22 +0530

    commit: home screen initialized

---
+++ lib/src/features/home/presentation/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('chumma music'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        onPressed: () {  },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
