bd9298130afb8b1d4fe51e67f70023ade9a7d9eb
Prathap <prathap@thirantech.com>
2024-12-21 13:33:49 +0530

    add: song model

---
+++ lib/src/features/home/domain/song_model.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'song_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SongImpl _$$SongImplFromJson(Map<String, dynamic> json) => _$SongImpl(
+      songName: (json['songname'] as num?)?.toInt(),
+      artist: json['artist'] as String?,
+      imageUrl: json['imageurl'] as String?,
+    );
+
+Map<String, dynamic> _$$SongImplToJson(_$SongImpl instance) =>
+    <String, dynamic>{
+      'songname': instance.songName,
+      'artist': instance.artist,
+      'imageurl': instance.imageUrl,
+    };
